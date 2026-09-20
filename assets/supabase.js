5a3ad8ea3de15c53cac8243aa2a63243cbbc2792
essa270 <essay270@gmail.com>
2026-09-20 06:19:39 +0300

    Initialize Supabase client in supabase.js

---
+++ assets/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://xklbpwazbzahenhusxsl.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_q87itJOVUmnh7gPoNI_1LA_b4h74uXE";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
